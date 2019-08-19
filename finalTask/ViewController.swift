7a939a8a3ed127c117abe51a12d19b3576501402
“Makoto87” <makosinhori@gmail.com>
2019-08-19 10:00:02 +0900

    画面起動時のview

---
+++ finalTask/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  finalTask
+//
+//  Created by VERTEX20 on 2019/08/18.
+//  Copyright © 2019 VERTEX20. All rights reserved.
+//
+
+// 起動画面(スプラッシュ)
+// ストーリーボード・・・タイムライン、地図、メッセージ、プロフィール、検索、
+// view・・・プロフィール(ツイッター)、通知、投稿
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
