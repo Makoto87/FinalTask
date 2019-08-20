8e509c39c553dea24a9d1e4afe32aa4b52f61acb
“Makoto87” <makosinhori@gmail.com>
2019-08-20 18:27:13 +0900

    投稿遷移画面作り直し

--- finalTask/PostViewController.swift
+++ finalTask/PostViewController.swift
@@ -10,30 +10,31 @@ import UIKit
 
 class PostViewController: UIViewController {
 
-    // アイコンのimageView
-    @IBOutlet weak var iconImageView: UIImageView!
-    // 場所のテキストフィールド
-    @IBOutlet weak var placeTextField: UITextField!
-    // 時間のテキストフィールド
-    @IBOutlet weak var timeTextField: UITextField!
-    // カテゴリーのテキストフィールド
-    @IBOutlet weak var categoryTextField: UITextField!
-    // 価格のテキストフィールド
-    @IBOutlet weak var priceTextField: UITextField!
-    // フリーコメントのテキストビュー
-    @IBOutlet weak var commentTextView: UITextView!
-    // 色を変えるために用意した投稿ボタンのoutlet
-    @IBOutlet weak var postAppearButton: UIButton!
+//    // アイコンのimageView
+//    @IBOutlet weak var iconImageView: UIImageView!
+//    // 場所のテキストフィールド
+//    @IBOutlet weak var placeTextField: UITextField!
+//    // 時間のテキストフィールド
+//    @IBOutlet weak var timeTextField: UITextField!
+//    // カテゴリーのテキストフィールド
+//    @IBOutlet weak var categoryTextField: UITextField!
+//    // 価格のテキストフィールド
+//    @IBOutlet weak var priceTextField: UITextField!
+//    // フリーコメントのテキストビュー
+//    @IBOutlet weak var commentTextView: UITextView!
+//    // 色を変えるために用意した投稿ボタンのoutlet
+//    @IBOutlet weak var postAppearButton: UIButton!
+
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // コメント欄の枠線設定
-        commentTextView.layer.borderWidth = 1.0
-        commentTextView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        commentTextView.layer.cornerRadius = 10.0
-
-        placeTextField.isEnabled = true
+//        commentTextView.layer.borderWidth = 1.0
+//        commentTextView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+//        commentTextView.layer.cornerRadius = 10.0
+//
+//        placeTextField.isEnabled = true
 
 
         // 入力判定するもの
@@ -47,34 +48,41 @@ class PostViewController: UIViewController {
             return
         }
         if placeTextField.text != nil {
-            postAppearButton.setTitleColor(UIColor.blue, for: .normal)
+    //        postAppearButton.setTitleColor(UIColor.blue, for: .normal)
         }
     }
 
     func textFieldDidBeginEditing(_ textField: UITextField) {
         // ボタンの色を変える処理
-        postAppearButton.setTitleColor(UIColor.blue, for: .normal)
+   //     postAppearButton.setTitleColor(UIColor.blue, for: .normal)
     }
 
     override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
         // ボタンの色を変える処理
-        postAppearButton.setTitleColor(UIColor.blue, for: .normal)
+  //      postAppearButton.setTitleColor(UIColor.blue, for: .normal)
     }
 
     // 開いた瞬間キーボードが表示される
     override func viewWillAppear(_ animated: Bool) {
-        timeTextField.becomeFirstResponder()
+    //    timeTextField.becomeFirstResponder()
     }
 
-    // キャンセルボタン
-    @IBAction func canselButton(_ sender: Any) {
-        view.backgroundColor = .blue
+//    // キャンセルボタン
+//    @IBAction func canselButton(_ sender: Any) {
+//        view.backgroundColor = .blue
+//    }
+//
+//    // 投稿ボタン
+//    @IBAction func postButton(_ sender: Any) {
+//
+//    }
+//
+
+    @IBAction func button(_ sender: Any) {
+    view.backgroundColor = UIColor.yellow
     }
 
-    // 投稿ボタン
-    @IBAction func postButton(_ sender: Any) {
 
-    }
 
 
 }
